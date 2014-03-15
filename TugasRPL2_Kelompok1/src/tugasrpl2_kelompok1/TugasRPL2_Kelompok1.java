eba039318775b0e695cb8c4339bd460422cf5621
helmynix <helmynix@yahoo.com>
2014-03-15 10:06:27 +0700

    versi 1
    tugas versi 1

---
+++ TugasRPL2_Kelompok1/src/tugasrpl2_kelompok1/TugasRPL2_Kelompok1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasrpl2_kelompok1;
+
+/**
+ *
+ * @author Helmy Faisal M <helmynix@gmail.com>
+ */
+public class TugasRPL2_Kelompok1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("membuat project netbeans");
+    }
+}
